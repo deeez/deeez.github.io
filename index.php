778d53b68764d2b05d73e90fdcb98206049aedfe
deeez <colorburn@gmail.com>
2016-06-07 20:07:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>DW</title>
+    <link href="main.css" rel="stylesheet">
+    <link href="css/scrolling-nav.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <section id="top">
+  <div class="billboard">
+  </div>
+  </section>
+  <!-- Navbar Start -->
+  <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container"> <!-- start -->
+  <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  </button>
+  <div class="collapse navbar-collapse navHeaderCollapse">
+  <ul class="nav navbar-nav navbar-right page-scroll">
+    <li class="active"><a class="page-scroll" href="#top">DW</a></li>
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle = "dropdown">Themes <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a class="page-scroll" href="#intimo">Intimo</a></li>
+          <li><a class="page-scroll" href="#dissonante">Dissonante</a></li>
+          <li><a class="page-scroll" href="#mezzo">Mezzo</a></li>
+        </ul>
+    </li>
+  </ul>
+  </div> <!-- end .container -->
+  </nav>
+  <!-- Navbar End -->
+  <!-- Blurb Start -->
+  <div class="blurb">
+  <div class="container">
+  <div class="row">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h4>Need a new theme?</h4><br><br>
+    <p>Why make a mediocre theme look nice when you can start with a pretty new theme?</p>
+  </div></div></div></div>
+  <!-- Blurb End -->
+  <!-- Thumbs Start -->
+  <div class="thumbs">
+  <div class="container">
+  <div class="row">
+  <!-- Intimo -->
+  <div class="col-md-4">
+    <center><h4>Intimo</h4></center>
+  <div class="thumbnail">
+    <a class="page-scroll" href="#intimo">
+      <img src="_intimo.jpg">
+    </a>
+  </div></div>
+  <!-- Dissonante -->
+  <div class="col-md-4">
+    <center><h4>Dissonante</h4></center>
+  <div class="thumbnail">
+    <a class="page-scroll" href="#dissonante">
+      <img src="_dissonante.jpg">
+    </a>
+  </div></div>
+  <!-- Mezzo -->
+  <div class="col-md-4">
+    <center><h4>Mezzo</h4></center>
+  <div class="thumbnail">
+    <a class="page-scroll" href="#mezzo">
+      <img src="_mezzo.jpg">
+    </a>
+  </div></div>
+  </div></div></div>
+  <!-- Thumbs End -->
+  <!-- Themes Start -->
+  <div class="themes">
+  <div class="container">
+  <!-- Intimo -->
+<section id="intimo">
+  <div class="row">
+  <div class="col-md-1"></div>
+  <div class="col-md-10">
+    <center><br><br><br><h4>Intimo</h4><br></center>
+  <div class="thumbnail">
+    <a href="http://intimo.derykwen.com" target="_blank">
+      <img src="_intimo.jpg">
+    </a>
+  </div></div></div>
+</section>
+  <!-- Dissonante -->
+<section id="dissonante">
+  <div class="row">
+  <div class="col-md-1"></div>
+  <div class="col-md-10">
+    <center><br><br><br><h4>Dissonante</h4><br></center>
+  <div class="thumbnail">
+    <a href="http://dissonante.derykwen.com" target="_blank">
+      <img src="_dissonante.jpg">
+    </a>
+  </div></div></div>
+</section>
+  <!-- Mezzo -->
+<section id="mezzo">
+  <div class="row">
+  <div class="col-md-1"></div>
+  <div class="col-md-10">
+    <center><br><br><br><h4>Mezzo</h4><br></center>
+  <div class="thumbnail">
+    <a href="http://mezzo.derykwen.com" target="_blank">
+      <img src="_mezzo.jpg">
+    </a>
+  </div></div></div>
+</section>
+  </div></div>
+  <!-- Themes End -->
+  <!-- Footer Start -->
+  <div class="footer">
+  <div class="container">
+    <center><p>© 2015 DW</p></center>
+  </div></div>
+  <!-- Footer End -->
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="js/scrolling-nav.js"></script>
+  <script type="text/javascript" src="js/jquery.easing.min.js"></script>
+  </body>
+</html>
